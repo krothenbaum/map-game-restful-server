65ae5717e9e61d5ea7877f289bf8d1e9c6b1dcda
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-09-03 17:45:46 -0700

    re-add models with lowercase

---
+++ app/models/city.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import Constants from '../config/constants';
+
+
+const CitySchema = new mongoose.Schema({
+    lat: Number,
+    lon: Number,
+    wikipedia: String,
+    city: String
+});
+
+const CityModel = mongoose.model('City', CitySchema);
+export default CityModel
---
+++ app/models/score.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import Constants from '../config/constants';
+
+
+const ScoreSchema = new mongoose.Schema({
+	score: Number,
+	name: String
+});
+
+const ScoreModel = mongoose.model('Score', ScoreSchema);
+export default ScoreModel;
